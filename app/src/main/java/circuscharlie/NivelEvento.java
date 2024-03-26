4aeba78cd071a6f7d5960f2d636543eba87fb93c
david99pereyra <david99pereyra@gmail.com>
2024-03-26 16:51:41 -0300

    Cree las clases Ranking, pelota, trampolin, monoazul, mononormal, bonus

---
+++ app/src/main/java/circuscharlie/NivelEvento.java
@@ -0,0 +1,15 @@
+package circuscharlie;
+import java.awt.*;
+public class NivelEvento {
+    private int nivelActual;
+    private Image image;
+
+
+    public void setNivel(){}
+    
+    //ver
+    public void cargarNivel(int numNivel){}
+
+    public void actualizarLogica(){}
+    public void finalDeNivel(){}
+}
