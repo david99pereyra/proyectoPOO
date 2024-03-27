b26f2855d129de28dfdeb8955379f827e0a14dfc
felisantillan00 <felipe.santillan00@gmail.com>
2024-03-27 20:26:47 -0300

    mod en jugador

--- app/src/main/java/circuscharlie/Jugador.java
+++ app/src/main/java/circuscharlie/Jugador.java
@@ -3,21 +3,29 @@ package circuscharlie;
 public class Jugador {
     private String nombre;
     private int puntuacion;
+    private int vidas;
+    private int nivel;
+
+    public Jugador(String nombre){
+        this.nombre = nombre;
+        this.puntuacion = 0;
+        this.vidas = 3;
+        this.nivel = 1;
+    }
 
     public String getNombre() {
         return this.nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void actualizarPuntaje(int puntos){
+        puntuacion += puntuacion;
     }
 
-    public int getPuntuacion() {
-        return this.puntuacion;
+    public void perderVida(){
+        vidas--;
     }
 
-    public void setPuntuacion(int puntuacion) {
-        this.puntuacion = puntuacion;
+    public void avanzarNivel(){
+        nivel++;
     }
-    
 }
