32c1e9b4d2c4a1a527651628ab14dc94e3fd84d9
JoaquinC2000 <joaquincenicero2000@hotmail.com>
2024-04-23 18:57:45 -0300

    Se agrego las clases: Fondo, Mundo, Camara, ObjetoGrafico

--- app/src/main/java/circuscharlie/Juegos/CircusCharlie/ObstaculosEventoCarrera.java
+++ app/src/main/java/circuscharlie/Juegos/CircusCharlie/ObstaculosEventoCarrera.java
@@ -1,6 +1,9 @@
 package circuscharlie.Juegos.CircusCharlie;
 
 public class ObstaculosEventoCarrera extends ObjetoGrafico{
+    public ObstaculosEventoCarrera(String filename) {
+        super(filename);
+    }
     private int puntajeAro;
     private int puntajeFuego;
 }
